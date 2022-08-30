91e58867cdef51dbd5d766e3c9807634a84c689a
alex <aa.santos95@gmail.com>
2022-08-30 17:57:28 -0300

    used wayback machine to gather info from previous site

--- contemplada_aqui/contemplada_aqui/settings.py
+++ contemplada_aqui/contemplada_aqui/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = ''
+MEDIA_URL = ''
