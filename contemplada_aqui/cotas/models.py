e67d105e824d057071d37997bb0fa2857feac6cb
alexafonsodossantos <aa.santos95@gmail.com>
2022-08-30 11:57:53 -0300

    Creating models

--- contemplada_aqui/cotas/models.py
+++ contemplada_aqui/cotas/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Administradora:
+    id = models.BigAutoField(primary_key=True)
+    nome = models.CharField(max_length=140)
+    img = odels.ImageField("Logo Administradora", upload_to='cotas/static/cotas/images', null=True)
+    def __str__(self):
+        return str(self.img.url)
+
+class Cota(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    codigo = models.CharField(max_length=140)
+    administradora = models.CharField(max_length=140)
+    valor = models.FloatField()
+    entrada = models.FloatField()
+    parcelas = models.IntegerField()
+    segmento = models.ImageField("Product Image", upload_to='loja/static/loja/images', null=True)
+    vencimento = models.TextField()
+    img = models.ForeignKey(Administradora, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.codigo)
