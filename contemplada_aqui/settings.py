b345e90df9934e9071a27db812b5eacc64b331a1
alexafonsodossantos <aa.santos95@gmail.com>
2022-10-14 19:26:17 -0300

    changed database credentials to environment variables

--- contemplada_aqui/settings.py
+++ contemplada_aqui/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'contemplada_aqui.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': = (os.environ.get("DATABASE_NAME")),
-        'USER': = (os.environ.get("DATABASE_USER")),
-        'PASSWORD': = (os.environ.get("DATABASE_PASSWORD")),
-        'HOST': = (os.environ.get("DATABASE_HOST")),
-        'PORT': = (os.environ.get("DATABASE_PORT")),
+        'NAME': = os.environ.get("DATABASE_NAME"),
+        'USER': = os.environ.get("DATABASE_USER"),
+        'PASSWORD': = os.environ.get("DATABASE_PASSWORD"),
+        'HOST': = os.environ.get("DATABASE_HOST"),
+        'PORT': = os.environ.get("DATABASE_PORT"),
     }
 }
 
