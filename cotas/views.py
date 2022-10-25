d01910ac4ffebda4649a6908c5416011726f79fc
alexafonsodossantos <aa.santos95@gmail.com>
2022-10-25 20:24:14 -0300

    instagram post test

--- cotas/views.py
+++ cotas/views.py
@@ -12,7 +12,50 @@ from rest_framework import generics
 from django.shortcuts import get_object_or_404, redirect
 from PIL import Image, ImageFont, ImageDraw
 import json
+import time
 # Create your views here.
+
+
+
+
+
+
+import requests
+import config
+import json
+def postInstagramQuote(filename):
+#Post the Image
+    image_location_1 = 'https://contempladaaqui.herokuapp.com/static/img/'+filename+'.png'
+    post_url = 'https://graph.facebook.com/v10.0/{}/media'.format('17841447246430902')
+    payload = {
+    'image_url': image_location_1,
+    'caption': 'Aqui seu dinheiro vale muito! Essa e outras oportunidades você encontra em www.contempladaaqui.com.br',
+    'access_token': 'EAAGCdVQMxg4BADw9Dakw3I9wZB10EPYHjZAhyr9f7RQQoT6sw9NPDANDL5GUM1WGKJZC9NSKruZA1aRfRM0O4wZBxm7J7LILZCBRV2UGZAVmn4qRZBHVPvtZBryqG5M0AZBwJ17FQDp7B9JzrbzXZCbpIxPxAtn00BrLupOfKbrDZAMh8gZDZD'
+    }
+    r = requests.post(post_url, data=payload)
+    print(r.text)
+    result = json.loads(r.text)
+    if 'id' in result:
+        creation_id = result['id']
+        second_url = 'https://graph.facebook.com/v10.0/{}/media_publish'.format('17841447246430902')
+        second_payload = {
+        'creation_id': creation_id,
+        'access_token':'EAAGCdVQMxg4BADw9Dakw3I9wZB10EPYHjZAhyr9f7RQQoT6sw9NPDANDL5GUM1WGKJZC9NSKruZA1aRfRM0O4wZBxm7J7LILZCBRV2UGZAVmn4qRZBHVPvtZBryqG5M0AZBwJ17FQDp7B9JzrbzXZCbpIxPxAtn00BrLupOfKbrDZAMh8gZDZD'
+        }
+        r = requests.post(second_url, data=second_payload)
+        print('--------Just posted to instagram--------')
+        print(r.text)
+    else:
+        print('HOUSTON we have a problem')
+
+
+
+
+
+
+
+
+
 
 locale.setlocale(locale.LC_MONETARY, "pt_BR.UTF-8")
 def update_agent(request):
@@ -221,24 +264,12 @@ def dashboard_create_template(request, pk):
 
         background.save('cotas/static/img/'+str(cota.codigo)+".png")
 
-        image_location_1 = 'https://contempladaaqui.herokuapp.com/static/img/'+str(cota.codigo)+'.png'
-        post_url = 'https://graph.facebook.com/v10.0/{}/media'.format('17841447246430902')
-        payload = {
-        'image_url': image_location_1,
-        'caption': 'Aqui seu dinheiro vale muito! Essa e outras oportunidades você encontra em www.contempladaaqui.com.br',
-        'access_token': 'EAAGCdVQMxg4BADw9Dakw3I9wZB10EPYHjZAhyr9f7RQQoT6sw9NPDANDL5GUM1WGKJZC9NSKruZA1aRfRM0O4wZBxm7J7LILZCBRV2UGZAVmn4qRZBHVPvtZBryqG5M0AZBwJ17FQDp7B9JzrbzXZCbpIxPxAtn00BrLupOfKbrDZAMh8gZDZD'
-        }
-        r = requests.post(post_url, data=payload)
-        result = json.loads(r.text)
-        if 'id' in result:
-            creation_id = result['id']
-            second_url = 'https://graph.facebook.com/v10.0/{}/media_publish'.format('17841447246430902')
-            second_payload = {
-            'creation_id': creation_id,
-            'access_token':'EAAGCdVQMxg4BADw9Dakw3I9wZB10EPYHjZAhyr9f7RQQoT6sw9NPDANDL5GUM1WGKJZC9NSKruZA1aRfRM0O4wZBxm7J7LILZCBRV2UGZAVmn4qRZBHVPvtZBryqG5M0AZBwJ17FQDp7B9JzrbzXZCbpIxPxAtn00BrLupOfKbrDZAMh8gZDZD'
-            }
-            r = requests.post(second_url, data=second_payload)
-            print(r.text)
+        time.sleep(3)
+
+        try:
+            postInstagramQuote(str(cota.codigo))       
+        except:
+            return HttpResponse("Ih rapaz...")
 
 
 
